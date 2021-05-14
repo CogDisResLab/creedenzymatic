083f8f43765b6a44d7dbf044dd249df2c9494532
Justin Creeden <61983016+JustinCreeden@users.noreply.github.com>
2021-05-14 15:18:59 -0400

    JFC test run

--- jfc/step_3_input.R
+++ jfc/step_3_input.R
@@ -1,7 +1,7 @@
 # Header Start ---------------------------
 # User: Justin Creeden / justincreeden@gmail.com
 # Date created: 2021_05_13
-# Script: Load required packages
+# Script: import input data
 # Purpose: Testing
 # Notes (if any): This script is part of test run
 #
@@ -9,5 +9,19 @@
 # Header End ---
 
 
-library(creedenzymatic)
-library(tidyverse)
+#Define variables
+JFC_directory_uka_input <- "jfc/input/hinds/2021-03-04-stk-h_f-v-h_c-UKA-Summaryresults 20210304-1330.txt"
+
+JFC_directory_krsa_input <- "jfc/input/hinds/2021-05-14-stk-h_f-v-h_c-acrossChip_KRSA_FullTable_comp1.txt"
+
+#Khaled, I changed your original code from 'median' to 'mean'
+JFC_desired_UKA_metric <- "Mean Final Score"
+#JFC_desired_UKA_metric <- "Mean Kinase Statistic"
+
+#Import and process
+krsa_ex <- read_delim(JFC_directory_krsa_input, delim = "\t")
+krsa_ex %>% select(Kinase, Z) %>% rename(Score = Z) -> krsa_ex
+uka_ex <- read_delim(JFC_directory_uka_input, delim = "\t")
+uka_ex %>% select("Kinase Name", JFC_desired_UKA_metric) %>% rename(Kinase = "Kinase Name", Score = JFC_desired_UKA_metric) -> uka_ex
+
+
